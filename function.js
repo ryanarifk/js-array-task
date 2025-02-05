4f828d8d7c53eeddf1d0a29c1ee32e25c78ee84a
Your Name <ryanarif7789@gmail.com>
2025-02-05 10:06:50 +0600

    function added

---
+++ function.js
@@ -0,0 +1,14 @@
+function timeHalf(number){
+    const half=number+20;
+    console.log(half);
+}
+
+timeHalf(15);
+
+function age(fatherAge,myAge){
+    const diffrance=fatherAge-myAge;
+    console.log('diffarence is',diffrance);
+}
+
+
+age(30,10);
